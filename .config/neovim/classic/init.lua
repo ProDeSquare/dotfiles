6bf4ffd54265f6b458b718f78b23976f693f517b
Hamza Mughal <hamza@prodesquare.com>
2022-03-05 19:25:33 +0500

    neovim: fixed keymap function calls

--- .config/neovim/classic/init.lua
+++ .config/neovim/classic/init.lua
@@ -39,31 +39,31 @@ local keymap = function(mode, key, result)
     vim.api.nvim_set_keymap(mode, key, result, { noremap = true, silent = true })
 end
 
-keymap("n", "<leader>w", ":w!<CR>", opts)
+keymap("n", "<leader>w", ":w!<CR>")
 
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
-keymap("n", "<Tab>", ">>_", opts)
-keymap("n", "<S-Tab>", "<<_", opts)
+keymap("n", "<Tab>", ">>_")
+keymap("n", "<S-Tab>", "<<_")
 
-keymap("n", "S", ":%s//g<Left><Left>", opts)
+keymap("n", "S", ":%s//g<Left><Left>")
 
-keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
-keymap("n", "<C-t>", "<cmd>Telescope live_grep<CR>", opts)
+keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>")
+keymap("n", "<C-t>", "<cmd>Telescope live_grep<CR>")
 
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>")
 
-keymap("n", "<leader>f", ":TZAtaraxis l15 r15<CR>", opts)
+keymap("n", "<leader>f", ":TZAtaraxis l15 r15<CR>")
 
-keymap("v", "<Tab>", ">gv", opts)
-keymap("v", "<S-Tab>", "<gv", opts)
-keymap("v", "p", '"_dP', opts)
+keymap("v", "<Tab>", ">gv")
+keymap("v", "<S-Tab>", "<gv")
+keymap("v", "p", '"_dP')
 
-keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
 -- autocmds
 vim.cmd[[
